9b38afe51744f616689ed4554dea3f0dda60f5b9
pani_marinka@ukr.net <wolf3812>
2021-07-26 18:18:32 +0300

    init2

--- src/main/java/enums/Enums.java
+++ src/main/java/enums/Enums.java
@@ -2,11 +2,11 @@ package enums;
 
 public enum Enums {
     SELECTMARK("You wont to work with BMW (1) or MERCEDES (2),  3 - Exit"),
-    ACTIVES("Select 1 - Add object  2 - View all cars 3 - Car Detail(idCar) 4 - Remove car(idCar) 5 - Sort car SpeedUp 6 - Sort car model  7 - Change mark car 8 - Exit "),
+    ACTIVES("Select 1 - Add car 2 - View all cars 3 - Car Detail(idCar) 4 - Remove car(idCar) 5 - Sort car SpeedUp 6 - Sort car model  7 - Change mark car 8 - Exit "),
     TRYMORE("INCORRET CHOICE"),
     MODEL("What model?"),
     SPEEDUP ("What SpeedUp?"),
-    SLOWDOWN ("What SlowDown&"),
+    SLOWDOWN ("What SlowDown?"),
     MILAGE ("What milage?"),
     MAXSPEED("What MaxSpeed?"),
     IDENTIFYCAR ("What identify Car (numsOfCar)?");
